84bc31057b057debf3ff17ea83391f5b472f6063
Khushi Agarwal <khushi19312@iiitd.ac.in>
2021-07-03 13:26:13 +0530

    screensharing implemented

--- public/script.js
+++ public/script.js
@@ -1,30 +1,32 @@
 const socket = io('/')
 const peer = new Peer(undefined
-//     , {
-//     host:'peerjs-server.herokuapp.com',
-//     port: '3001'
-// }
 )
-const videogrid = document.getElementById('video-grid')
+const videogrid = document.querySelector('.video-grid')
 const uservideo = document.createElement('video')
 uservideo.muted = true;
+let conn =null;
+let currentpeer;
+let userIdentity = null;
 let peers = {}
 let myvideostream;
-let callreceived=null;
+let callreceived;
+let senders = [];
+
 let promise = navigator.mediaDevices.getUserMedia({
     video: true, audio: true
 })
 promise.then((stream) => {
-    console.log('promise true')
+    // console.log('promise true')
     myvideostream = stream;
     addVideoStream(uservideo, stream)
-
     if(callreceived){
         callreceived.answer(myvideostream)
         const video = document.createElement('video')
         video.muted = true;
         callreceived.on('stream', peervideostream => {
             console.log('got video inside')
+            currentpeer = callreceived.peerConnection;
+            console.log(currentpeer);
             addVideoStream(video, peervideostream)
         })
     }
@@ -35,24 +37,19 @@ promise.then((stream) => {
         video.muted = true;
         call.on('stream', peervideostream => {
             console.log('got video inside')
+            currentpeer = call.peerConnection;
+            console.log(currentpeer);
             addVideoStream(video, peervideostream)
         })
     })
 })
 
 peer.on('call', (call) => {
-    // console.log('call outside', myvideostream)
-    // call.answer(myvideostream)
-    
-    // const video = document.createElement('video')
-    // video.muted = true;
-    // call.on('stream', peervideostream => {
-    //     console.log('got video outside')
-    //     addVideoStream(video, peervideostream)
-    // })
     callreceived = call;
+    currentpeer = call.peerConnection;
+    console.log(currentpeer);
 })
-let conn =null;
+
 socket.on('user-connected', (userId) => {
     console.log('connected')
     connectToNewUser(userId, myvideostream)
@@ -63,7 +60,7 @@ socket.on('user-disconnected', (userId) => {
     // console.log(userId);
     if (peers[userId]) peers[userId].close()
 })
-let userIdentity = null;
+
 peer.on('open', id =>{
     socket.emit('join-meet', MEET_ID, id)
     userIdentity = id;
@@ -76,20 +73,104 @@ const connectToNewUser = (userId, stream) => {
     call.on('stream', (peervideostream) => {
         console.log('got video in connectNewUser')
         addVideoStream(video, peervideostream)
+        currentpeer = call.peerConnection;
     })
     call.on('close', ()=>{
         video.remove()
     })
     peers[userId] = call;
 }
+
 const addVideoStream = (video, stream) => {
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
-        console.log('rendering video')
-        videogrid.append(video)
+        console.log('rendering video ', Object.keys(peers).length)
+        let cont = document.createElement('div');
+        cont.className = 'container';
+        videogrid.append(cont);
+        cont.append(video);
+        layout();
     })
 }
+const layout = ()=>{
+    var num = Object.keys(peers).length + 1; 
+    console.log(num);
+    let w='800px';
+    let h='auto';
+    let col= 1;
+    if (num > 1 && num <= 4) { 
+        w = '500px';
+        h = 'auto';
+        col=2;
+
+    } else if (num > 4) {
+        // rowHeight = '24vh';
+        w = '300px';
+        h = 'auto';
+        col=3;
+    }
+    document.querySelector(".video-grid").style.setProperty('--width', w);
+    document.querySelector(".video-grid").style.setProperty('--height', h);
+    document.querySelector(".video-grid").style.setProperty('--cols', col);
+}
+
+//controls
+const audio= document.querySelector(".audio");
+const video= document.querySelector(".video");
+const screenshare= document.querySelector(".screenshare");
+let videoflag=0;
+let audioflag=0;
+audio.addEventListener("click", ()=>{
+    console.log('heard audio toggle')
+    myvideostream.getAudioTracks()[0].enabled = !(myvideostream.getAudioTracks()[0].enabled);
+    if(audioflag===0){
+        audio.style.backgroundImage = 'url("/icons/icons8-mute-unmute-96.png")';
+        audioflag=1;
+    }
+    else{
+        audio.style.backgroundImage = 'url("/icons/icons8-microphone-96.png")';
+        audioflag=0;
+    }
+})
+video.addEventListener("click", ()=>{
+    console.log('heard video toggle')
+    myvideostream.getVideoTracks()[0].enabled = !(myvideostream.getVideoTracks()[0].enabled);
+    if(videoflag===1){
+        video.style.backgroundImage = 'url("/icons/video-camera-close.png")';
+        videoflag=0;
+    }
+    else{
+        video.style.backgroundImage = 'url("/icons/video-camera.png")';
+        videoflag=1;
+    }
+})
+screenshare.addEventListener("click", (event)=>{
+    let displaypromise = navigator.mediaDevices.getDisplayMedia({
+        video: {cursor: "always"},
+        audio: {echocancellation: true, noiseSuppression: true}
+    });
+    displaypromise.then((stream)=>{
+        // peer.call(userIdentity, stream);
+        let videoTrack = stream.getVideoTracks()[0];
+        console.log(currentpeer);
+        let screen = document.createElement("video")
+        addVideoStream(screen, stream);
+        videoTrack.onended = ()=>{
+            let videoTrack = myvideostream.getVideoTracks()[0];
+            let sender = currentpeer.getSenders().find((s)=>{
+                return s.track.kind == videoTrack.kind
+            })
+            sender.replaceTrack(videoTrack)
+            screen.remove()
+        }
+        let sender = currentpeer.getSenders().find((s)=>{
+            return s.track.kind == videoTrack.kind
+        })
+        sender.replaceTrack(videoTrack)
+        
+    })
+})
 
 //chat
 const send = document.querySelector("#send");
@@ -108,4 +189,6 @@ socket.on('broadcastMessage', (data)=>{
     console.log('client side ', data.message);
     output.innerHTML += '<p><strong>' + data.user.slice(0,6) + ': </strong><br>' + data.message + '</p>';
 })
+
+
 
